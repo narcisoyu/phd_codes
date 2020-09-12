fea3c78074846bff656e6250aac4e7cbf6df0c81
alexziver <narcisoyu@gmail.com>
2020-09-12 11:41:18 +0200

    Update image_network.R

--- visual_network/image_network.R
+++ visual_network/image_network.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(igraph)
 library(rgexf)
 library(png)
+library(ggraph)
+#library(patchwork)
 #library(jpeg)
 
 data <- read_excel("Dataset_VisualSemCovid.xlsx", sheet = "VisualSemCovid")
@@ -67,7 +69,47 @@ for (i in 2:length(nodes)) {
 
 #V(g2)$raster[[2]] <- pic3
 
-plot(g2, vertex.shape='raster', vertex.label = NA, layout = LO, edge.width = 5, edge.color = "blue")
+plot(g2, vertex.shape='raster', vertex.label = NA, layout = LO, edge.width = 5, edge.color = "black")
 
 ##save g2 to gephi?
 #igraph.to.gexf(g2)
+
+
+
+
+
+###ANOTHER IDEA
+#create frame network from the matrix 'df'
+##CREATE A CO-OCCURRENCE (frame) MATRIX FROM THE 'df'
+M <- crossprod(df)
+diag(M) <- 0
+g_frame <- graph_from_adjacency_matrix(M, mode = c("undirected"), weighted = TRUE, diag = FALSE)
+plot(g_frame)
+###filter graph by vertex degree
+del_frame = which(degree(g_frame) < 1)
+g_frame_2 = delete.vertices(g_frame, del_frame)
+plot(g_frame_2)
+
+
+
+##add node strength
+
+
+set.seed(5200)
+
+g_frame_3 <- g_frame_2 %>%  
+    ggraph(layout = 'drl') +
+    #scale_label_size(minor_breaks = NULL) +
+    geom_edge_link(aes(edge_width = E(g_frame_2)$weight, alpha = E(g_frame_2)$weight)) +
+    scale_edge_width(range = c(0.1, 3))+ # control size
+    #geom_edge_link(aes(alpha = weight)) +
+    geom_node_point(aes(size = strength(g_frame_2), alpha = strength(g_frame_2))) + scale_size(range = c(1,15)) +
+    geom_node_label(aes(label = name), repel = TRUE)+
+    #geom_node_label(aes(label = id.aff, size = peso))+
+    theme_graph() +
+    labs(title = c("Frame network")) +
+    theme(legend.position = "none") #if hide legend, add this line
+    #labs(subtitle = my.period[nperiod])
+
+g_frame_3
+
